894c2652c39634b8d570e64b15c2474585b2cdbe
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2023-06-10 17:51:54 +0900

    画像が砂のように消滅するシェーダーの作成

---
+++ Assets/Shader/Vanishing/VanishingShaderController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class VanishingShaderController : MonoBehaviour
+{
+    [SerializeField] float strengthX = 0f;
+    [SerializeField] float strengthY = 0f;
+    [SerializeField] float alpha = 1f;
+    [SerializeField] Material material;
+
+    void Update()
+    {
+        UpdateMaterial();
+    }
+
+    void UpdateMaterial()
+    {
+        if (material == null) { return; }
+
+        material.SetFloat("_StrengthX", strengthX);
+        material.SetFloat("_StrengthY", strengthY);
+        material.SetFloat("_Alpha", alpha);
+    }
+}
